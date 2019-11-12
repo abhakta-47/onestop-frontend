4e0f4eed4e5a88651e73290018db6443fc019991
Romit Karmakar <romitkarmakar@gmail.com>
2019-11-12 16:36:44 +0530

    Added component

---
+++ components/AddWorkshop.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="form-group">
+        <input type="text" class="form-control">
+      </div>
+      <div class="form-group">
+        <textarea type="text" class="form-control" rows="3">
+      </div>
+      <a-date-picker @change="onChange" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { DatePicker } from 'ant-design-vue';
+Vue.use(DatePicker);
+
+import 'ant-design-vue/dist/antd.css';
+export default {
+methods: {
+  onChange(date, dateString) {
+        console.log(date, dateString);
+      }
+}
+}
+</script>
