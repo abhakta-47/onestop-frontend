a627a453ff1760b958469e09c2abb9281a97838c
SucharuRai <raisucharugzp@gmail.com>
2019-11-15 00:55:00 +0530

    Added axios and some changes

--- components/AddWorkshop.vue
+++ components/AddWorkshop.vue
@@ -14,15 +14,22 @@
 
 <script>
 import Vue from 'vue';
+import axios from 'axios';
 import { DatePicker } from 'ant-design-vue';
 Vue.use(DatePicker);
 
 import 'ant-design-vue/dist/antd.css';
 export default {
-methods: {
-  onChange(date, dateString) {
-        console.log(date, dateString);
-      }
-}
+  methods: {
+    onChange(date, dateString) {
+          console.log(date, dateString);
+        },
+    submit() {
+      var self = this;
+      axios.post('https://dxepsmwpz6.execute-api.ap-south-1.amazonaws.com/production/profile', {
+        "email":self.email,"name":self.name,"rollNo":self.rollNo,"contactNo":self.contactNo
+      }).then(res => {})
+    }
+  }
 }
 </script>
